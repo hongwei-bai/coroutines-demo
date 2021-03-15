0cc9dd65a6a69dfd653ed1b2a36a87ba798ac00f
Hongwei <bhw8412@hotmail.com>
2021-03-16 07:30:36 +1100

    add call api from remote

--- app/src/main/java/com/hongwei/coroutines_demo/constants/DemoEndpoint.kt
+++ app/src/main/java/com/hongwei/coroutines_demo/constants/DemoEndpoint.kt
@@ -1,5 +1,6 @@
 package com.hongwei.coroutines_demo.constants
 
 object DemoEndpoint {
-    const val baseUrl = "http://10.0.2.2:8080"
+//    const val baseUrl = "http://10.0.2.2:8080"
+    const val baseUrl = "http://39.100.128.234/mock-server-coroutines-demo/"
 }
